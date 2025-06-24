ac342f57d87c991d91f2bb21b51dcfc1e233e28a
Nathan Barbet <71265780+Darknath78@users.noreply.github.com>
2025-06-24 16:35:52 +0200

    [ADD] Logo

--- Football_Manager/footer.php
+++ Football_Manager/footer.php
@@ -1,7 +1,7 @@
 </main>
 
 <footer style="text-align: center; padding: 20px; margin-top: 40px; background-color: #e0e0e0;">
-    <p>&copy; <?php echo date('Y'); ?> - Projet SAE23 - Football_Manager - Nathan Barbet, Nadir Vassili</p>
+    <p>&copy; <?php echo date('Y'); ?> - Projet SAE23 - Football_Manager - Nathan Barbet</p>
 </footer>
 
 </body>
